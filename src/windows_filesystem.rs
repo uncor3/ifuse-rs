a83d9445f6e2ded8e61ad9205a4959d6e8ba96d2
uncor3 <contactuncor3@gmail.com>
2026-08-03 04:44:40 +0300

    fix windows build

--- src/windows_filesystem.rs
+++ src/windows_filesystem.rs
@@ -24,6 +24,7 @@ use winfsp::{
     FspError,
     filesystem::{
         DirInfo, DirMarker, FileInfo, FileSecurity, FileSystemContext, OpenFileInfo, VolumeInfo,
+        WideNameInfo,
     },
 };
 
